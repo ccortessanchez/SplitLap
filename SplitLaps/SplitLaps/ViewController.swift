34004ec4e7e219ac9e10e346873d08789909ceaa
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-12-15 14:08:12 +0100

    Add timer

--- SplitLaps/SplitLaps/ViewController.swift
+++ SplitLaps/SplitLaps/ViewController.swift
@@ -12,13 +12,20 @@ import RxCocoa
 
 class ViewController: UIViewController {
     
-    var timer: Observable<NSInteger>!
-    
     @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var splitBtn: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var disposeBag = DisposeBag()
+    var timer: Observable<NSInteger>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timer = Observable<NSInteger>
+            .interval(0.1, scheduler: MainScheduler.instance)
+        timer.subscribe(onNext: { msecs -> Void in
+            print("\(msecs)00ms")
+        }).disposed(by: disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
